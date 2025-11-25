79b6624add40f7fd186a4ba45038e0a57db8d631
ellii <patrick@savells.net>
2025-11-24 18:14:04 -0600

    updated expandablility

--- fake-sigs/main.cpp
+++ fake-sigs/main.cpp
@@ -1,60 +1,54 @@
 #include <windows.h>
-#include <iostream>
+#include <intrin.h>
 
-#define FAKE_JUNK do {            \
-    volatile int a = 123;         \
-    volatile int b = 456;         \
-    volatile int c = 0;           \
-    for (int i = 0; i < 10; ++i) {   \
-        a ^= b;                  \
-        b += i;                  \
-        c += (a & b) ^ (b | a); \
-    }                            \
-    if ((c % 3) == 0) {          \
-        c = (c << 2) | (c >> 3);\
-    } else {                     \
-        c = (c >> 1) ^ (a + b); \
-    }                            \
-    for (int i = 0; i < 5; ++i) {\
-        __nop;               \
-    }                            \
+#define RAND_SEED (__COUNTER__ * 1234567 + __LINE__)
+#define RND(min, max) (min + (RAND_SEED % (max - min + 1)))
+
+#define JUNK_BODY() do {                          \
+    volatile int a = RND(100, 999);              \
+    volatile int b = RND(100, 999);              \
+    volatile int c = RND(1, 50);                 \
+                                                   \
+    for (int i = 0; i < RND(5, 25); ++i) {        \
+        a ^= (b + i);                              \
+        b += (a * i) ^ (c + 31);                   \
+        c ^= (a | b);                              \
+        b = (b << 1) | (b >> 3);                   \
+        a ^= (c << 2);                             \
+    }                                              \
+                                                   \
+    if ((a & 1) == 0) {                            \
+        b ^= (a << 1);                             \
+    } else {                                       \
+        a ^= (b >> 1);                             \
+    }                                              \
+                                                   \
+    for (int i = 0; i < RND(5,15); ++i) {          \
+        _mm_pause();                               \
+        __nop();                                   \
+    }                                              \
 } while(0)
 
-#pragma code_seg(".vmp0")
-__declspec(noinline) void VMP0() {
-    FAKE_JUNK;
-}
-#pragma code_seg()
+#define CREATESECTION(name) \
+__pragma(code_seg("." #name)) \
+__declspec(noinline) void name() { JUNK_BODY(); } \
+__pragma(code_seg())
 
-#pragma code_seg(".vmp1")
-__declspec(noinline) void VMP1() {
-    FAKE_JUNK;
-}
-#pragma code_seg()
+CREATESECTION(vmp0) 
+CREATESECTION(vmp1)
+CREATESECTION(enigma1)
+CREATESECTION(enigma2)
+CREATESECTION(themida)
+CREATESECTION(junkx)
 
-#pragma code_seg(".enigma1")
-__declspec(noinline) void ENGIMA1() {
-    FAKE_JUNK;
-}
-#pragma code_seg()
+int main()
+{
+    vmp0();
+    vmp1();
+    enigma1();
+    enigma2();
+    themida();
+    junkx();
 
-#pragma code_seg(".enigma2")
-__declspec(noinline) void ENGIMA2() {
-    FAKE_JUNK;
-}
-#pragma code_seg()
-
-#pragma code_seg(".themida")
-__declspec(noinline) void THEMIDA() {
-    FAKE_JUNK;
-}
-#pragma code_seg()
-
-int main() {
-    VMP0();
-    VMP1();
-    ENGIMA1();
-    ENGIMA2();
-    THEMIDA();
     return 0;
 }
